61f0859334cc24f8927c249f91235631e7399b24
GollaSanthosh123 <166037844+GollaSanthosh123@users.noreply.github.com>
2026-04-14 11:19:15 +0530

    health_score_ml.py

--- health_score_ml.py
+++ health_score_ml.py
@@ -1,16 +1,12 @@
 def calculate_health_score(row):
 
     score = 0
-
-    # Steps score
     if row["steps"] >= 8000:
         score += 50
     elif row["steps"] >= 5000:
         score += 30
     else:
         score += 10
-
-    # Heart rate score
     if 60 <= row["heart_rate"] <= 100:
         score += 50
     else:
